1ffcb1c5cd10feaddb2d6c3821338aa3b2bf8d40
Igdálias Mussulveira Cabamba <igdaliasmussulveiracabamba@gmail.com>
2023-01-20 00:03:01 +0100

    added new emulators; password input; and minor ui changes

--- app/src/ui/primary_screen.py
+++ app/src/ui/primary_screen.py
@@ -5,55 +5,68 @@ from PySide6.QtGui import QPixmap
 import qtawesome as qta
 
 
-class PrimaryScreen(QFrame):
+class Home(QFrame):
     def __init__(self, parent):
         super().__init__(parent)
         self.build()
 
     def build(self) -> None:
-        self.vbox = QVBoxLayout(self)
+        self.vbox = QHBoxLayout(self)
         self.vbox.setContentsMargins(0, 0, 0, 0)
         self.setLayout(self.vbox)
 
-        hbox1 = QHBoxLayout()
-        hbox1.setContentsMargins(0, 0, 0, 0)
-
-        self.main_widget = QFrame(self)
-        hbox1.addWidget(self.main_widget, Qt.AlignCenter)
-
-        self.vbox_main = QHBoxLayout(self.main_widget)
-        self.vbox_main.setContentsMargins(0, 0, 0, 0)
-        self.main_widget.setLayout(self.vbox_main)
-
         self.start_panel = QFrame(self)
-        vbox_panel = QVBoxLayout(self.start_panel)
-        vbox_panel.setContentsMargins(0, 0, 0, 0)
-        self.start_panel.setLayout(vbox_panel)
+        self.start_panel.setMinimumWidth(300)
+        self.vbox_panel = QVBoxLayout(self.start_panel)
+        self.vbox_panel.setContentsMargins(0, 0, 0, 0)
+        self.start_panel.setLayout(self.vbox_panel)
         self.start_panel.setSizePolicy(QSizePolicy(
             QSizePolicy.Fixed, QSizePolicy.Fixed))
+
+        self.user_panel = QFrame(self)
+        vbox_user_panel = QVBoxLayout(self.user_panel)
+        vbox_user_panel.setContentsMargins(5, 5, 5, 5)
+        self.user_panel.setLayout(vbox_user_panel)
+
+        self.password_input = QLineEdit(self)
+        self.password_input.setEchoMode(QLineEdit.Password)
+        self.password_input.setPlaceholderText("password")
+        vbox_user_panel.addWidget(self.password_input)
+        self.vbox_panel.addWidget(self.user_panel)
 
         self.btn_new_terminal = QPushButton(self)
         self.btn_new_terminal.setText("New terminal")
         self.btn_new_terminal.setObjectName("AppButtonPrimary")
-        self.btn_new_terminal.setSizePolicy(
-            QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed))
 
         self.btn_install_distro = QPushButton(self)
         self.btn_install_distro.setText("Install")
         self.btn_install_distro.setIcon(qta.icon('fa5s.download', options=[
                                         {'scale_factor': 1}], color='white'))
         self.btn_install_distro.setObjectName("AppButtonSecondary")
-        self.btn_install_distro.setSizePolicy(
-            QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed))
 
         hbox_panel1 = QHBoxLayout()
-        hbox_panel1.setContentsMargins(0, 0, 0, 0)
+        hbox_panel1.setContentsMargins(5, 0, 5, 0)
 
         hbox_panel1.addWidget(self.btn_new_terminal, Qt.AlignCenter)
         hbox_panel1.addWidget(self.btn_install_distro, Qt.AlignCenter)
 
-        vbox_panel.addLayout(hbox_panel1)
+        self.vbox_panel.addLayout(hbox_panel1)
 
-        self.vbox_main.addWidget(self.start_panel, Qt.AlignCenter)
+        self.vbox.addWidget(self.start_panel)
 
-        self.vbox.addLayout(hbox1)
+
+class PrimaryScreen(QFrame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.build()
+
+    def build(self) -> None:
+        self.box = QStackedLayout(self)
+        self.box.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(self.box)
+
+        self.home = Home(self)
+        self.btn_new_terminal = self.home.btn_new_terminal
+
+        self.box.addWidget(self.home)
+        self.box.setCurrentWidget(self.home)
--- thanks.md
+++ thanks.md
@@ -3,9 +3,6 @@
 <a target="_blank" href="https://icons8.com/icon/QEjCtdXzfP6B/expand-arrow">Expand Arrow</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
 <a target="_blank" href="https://icons8.com/icon/4511GGVppfIx/settings">Settings</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
 <a target="_blank" href="https://icons8.com/icon/jYAj1cNBAili/columns">Columns</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
-
-<a href="https://www.freepik.com/free-photo/minimal-globe-technology-business-background_15474113.htm#query=space&position=43&from_view=search&track=sph">Image by rawpixel.com</a> on Freepik
-
-<a href="https://www.freepik.com/free-photo/aesthetic-dark-wallpaper-background-neon-light_17581562.htm#query=space&position=2&from_view=search&track=sph">Image by rawpixel.com</a> on Freepik
-
 <a target="_blank" href="https://icons8.com/icon/101665/kali-linux">Kali Linux</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+<a target="_blank" href="https://icons8.com/icon/63208/ubuntu">Ubuntu</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+<a target="_blank" href="https://icons8.com/icon/17838/debian">Debian</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
